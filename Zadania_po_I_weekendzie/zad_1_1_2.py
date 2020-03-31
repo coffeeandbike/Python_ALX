6e07d5c943725eb258221d328bef7dd36238ca79
coffeeandbike <62751912+coffeeandbike@users.noreply.github.com>
2020-03-31 22:18:30 +0200

    Create zad_1_1_2.py

---
+++ Zadania_po_I_weekendzie/zad_1_1_2.py
@@ -0,0 +1,13 @@
+'''
+Potem napisz program, który prosi użytkownika (przez `input()`), żeby podał,
+ ile kosztuje kilo ziemniaków i ile kilo chce kupić. Niech program policzy i wyświetli,
+ ile trzeba będzie zapłacić za te ziemniaki.
+'''
+
+cena_za_kg = float(input('Ile kosztuje 1 kg ziemniakow ? '))
+
+ile_chcesz_kupic = float(input('Ile kg ziemniakow chcesz kupic ? '))
+
+cena_za_kupione_ziemniaki = cena_za_kg * ile_chcesz_kupic
+
+print(f'Za {ile_chcesz_kupic} kg ziemniakow bedziesz musial/a zaplacic:\n {cena_za_kupione_ziemniaki:<.2f} PLN')
