8e7740e72cc28e6ad599a8f5507c02edcdc7d857
coffeeandbike <coffeeandbike@users.noreply.github.com>
2020-03-31 22:08:47 +0200

    commit

--- Zadania_po_I_weekendzie/zad_1_2.py
+++ Zadania_po_I_weekendzie/zad_1_2.py
@@ -1,14 +1,68 @@
 '''
-Potem napisz program, który prosi użytkownika (przez `input()`), żeby podał,
- ile kosztuje kilo ziemniaków i ile kilo chce kupić. Niech program policzy i wyświetli,
- ile trzeba będzie zapłacić za te ziemniaki.
+Napisz taki program: użytkownik ma podać, w jaki dzień tygodnia oddał buty do szewca
+(poniedziałek to dzień 1, wtorek to dzień 2 itp.). Ma też podać, ile dni będzie trwała naprawa.
+​
+Program ma wypisać, w jaki dzień tygodnia buty będą gotowe do odbioru.
+Jeśli tak będzie ci wygodniej, możesz założyć, że naprawa butów nigdy nie będzie trwała dłużej niż siedem dni.
 '''
 
-cena_za_kg = float(input('Ile kosztuje 1 kg ziemniakow ? '))
+powitanie = "SUPER PROGRAM - KIEDY BUTY"
+print(f'{powitanie:_^40}')
+print()
+print("""Program obliczy, w ktory dzien tygodnia
+Twoje buty beda do odbioru, jesli podasz w nastepujace sposob
+dzien oddania butow do naprawy:
+Poniedzialek - 1
+Wtorek - 2
+Sroda - 3
+Czwartek -4
+Piatek -5
+Sobota - 6
+Niedziela -7""")
 
-ile_chcesz_kupic = float(input('Ile kg ziemniakow chcesz kupic ? '))
+dzien_oddania = int(input('Podaj w jaki dzien oddales buty do szewca: '))
 
-cena_za_kupione_ziemniaki = cena_za_kg * ile_chcesz_kupic
 
-print(f'Za {ile_chcesz_kupic} kg ziemniakow bedziesz musial/a zaplacic:\n {cena_za_kupione_ziemniaki:<.2f} PLN')
+ilosc_dni_naprawy = int(input('Ile dni bedzie trwala naprawa ? '))
 
+ilosc_tygodni = int(abs(ilosc_dni_naprawy / 7))
+ilosc_dni_w_tygodniu = (ilosc_dni_naprawy % 7)
+
+
+print()
+if ilosc_tygodni >= 1:
+    if ((dzien_oddania + 7) % 7) == 1:
+        print(f'Twoje buty beda gotowe w poniedzialek za {ilosc_tygodni} tygodni/e')
+    elif ((dzien_oddania + 7) % 7) == 2:
+        print(f'Twoje buty beda gotowe we wtorek za {ilosc_tygodni} tygodni/e')
+    elif ((dzien_oddania + 7) % 7) == 3:
+        print(f'Twoje buty beda gotowe w srode za {ilosc_tygodni} tygodni/e')
+    elif ((dzien_oddania + 7) % 7) == 4:
+        print(f'Twoje buty beda gotowe w czwartek za {ilosc_tygodni} tygodni/e')
+    elif ((dzien_oddania + 7) % 7) == 5:
+        print(f'Twoje buty beda gotowe w piatek za {ilosc_tygodni} tygodni/e')
+    elif ((dzien_oddania + 7) % 7) == 6:
+        print(f'Twoje buty beda gotowe w sobote za {ilosc_tygodni} tygodni/e')
+    elif ((dzien_oddania + 7) % 7) == 0:
+        print(f'Twoje buty beda gotowe w niedziele za {ilosc_tygodni} tygodni/e')
+
+
+if ilosc_tygodni == 0:
+    if (dzien_oddania + ilosc_dni_naprawy) % 7 == 2:
+        print('Twoje buty beda gotowe w najblizszy wtorek')
+    elif (dzien_oddania + ilosc_dni_naprawy) == 3 or 10:
+        print('Twoje buty beda gotowe w najblizsza srode')
+    elif (dzien_oddania + ilosc_dni_naprawy) == 4 or 11:
+        print('Twoje buty beda gotowe w najblizszy czwartek')
+    elif (dzien_oddania + ilosc_dni_naprawy) == 5 or 12:
+        print('Twoje buty beda gotowe w najblizszy piatek')
+    elif (dzien_oddania + ilosc_dni_naprawy) == 6 or 13:
+        print('Twoje buty beda gotowe w najblizsza sobote')
+    elif (dzien_oddania + ilosc_dni_naprawy) == 7 or 14:
+        print('Twoje buty beda gotowe w najblizsza niedziele')
+    elif (dzien_oddania + ilosc_dni_naprawy) == 8:
+        print('Twoje buty beda gotowe w najblizszy poniedzialek')
+print()
+
+pozegnanie = "ZAPRASZAMY PO ODBIOR, DO WIDZENIA"
+print(f'{pozegnanie:_^40}')
