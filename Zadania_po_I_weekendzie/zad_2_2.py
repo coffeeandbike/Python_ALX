0c764a08f19e172a4741165e830b4a35d990b277
coffeeandbike <coffeeandbike@users.noreply.github.com>
2020-04-12 18:46:47 +0200

    task done

---
+++ Zadania_po_I_weekendzie/zad_2_2.py
@@ -0,0 +1,24 @@
+"""
+
+Zadanie 2.2 | Choinka
+​
+Napisz program, który wczytuje liczbę całkowitą, a następnie na konsolę wypisuje w tylu liniach "choinkę" ze znaków `*`.
+Np. dla parametru 3 powinno się wypisać:
+​
+
+    *
+  * * *
+* * * * *
+"""
+
+ilosc_linijek = int(input("Podaj ilosc 'poziomow' choinki do wyswirtlenia: "))
+i = 1
+ilosc_znakow = 1
+
+najdluzszy_wiersz = (ilosc_linijek * 2) -1
+
+
+while i <= ilosc_linijek:
+    print(f"{'*' * ilosc_znakow:^{najdluzszy_wiersz}}", end="\n")
+    i += 1
+    ilosc_znakow += 2
