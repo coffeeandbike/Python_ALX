327766eb2966cde9662ad9e1f0ad3a19c84643ac
coffeeandbike <coffeeandbike@users.noreply.github.com>
2020-04-12 19:00:58 +0200

    task done

--- Zadania_po_I_weekendzie/zad_2_2.py
+++ Zadania_po_I_weekendzie/zad_2_2.py
@@ -10,15 +10,24 @@ Np. dla parametru 3 powinno się wypisać:
   * * *
 * * * * *
 """
+while True:
 
-ilosc_linijek = int(input("Podaj ilosc 'poziomow' choinki do wyswirtlenia: "))
-i = 1
-ilosc_znakow = 1
+    ilosc_linijek = int(input("Podaj ilosc 'poziomow' choinki do wyswirtlenia: "))
+    i = 1
+    ilosc_znakow = 1
 
-najdluzszy_wiersz = (ilosc_linijek * 2) -1
+    najdluzszy_wiersz = (ilosc_linijek * 2) -1
 
 
-while i <= ilosc_linijek:
-    print(f"{'*' * ilosc_znakow:^{najdluzszy_wiersz}}", end="\n")
-    i += 1
-    ilosc_znakow += 2
+    while i <= ilosc_linijek:
+        print(f"{'*' * ilosc_znakow:^{najdluzszy_wiersz}}", end="\n")
+        i += 1
+        ilosc_znakow += 2
+
+    co_dalej = input("Jesli chcesz narysowac kolejna choinke, wcisnij 'n' ")
+
+    if co_dalej == 'n':
+        continue
+    else:
+        print("KONIEC")
+        break
