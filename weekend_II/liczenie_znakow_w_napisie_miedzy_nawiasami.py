c27dd802e0a5194cf9d2e5d6becd0f2f55a911c1
coffeeandbike <coffeeandbike@users.noreply.github.com>
2020-04-18 09:52:35 +0200

    task finished

---
+++ weekend_II/liczenie_znakow_w_napisie_miedzy_nawiasami.py
@@ -0,0 +1,33 @@
+'''
+Napisz program zliczający liczbę znaków w podanym przez użytkownika napisie pomiędzy nawiasami <>.
+Nawiasy mogą wystąpić tylko raz.
+Ala ma <kota>, a kot ma Alę
+'''
+
+# 1 sprawdzam liczbe < i > - powinno byc po 1
+# 2 jak jest wiecej niz po 1 to koniec programy
+
+# 3 w petli sprawdzam czy:
+# mam zliczzac
+# mam przestac zliczac
+# czy zlicznie jest wlaczone i wtedy zliczam
+
+napis = input("Podaj napis: ")
+
+if napis.count('<') != 1 or napis.count('>') != 1:
+    print("Nieprawidlowa ilosc nawiasiow !!!")
+    exit()
+
+liczna_znakow = 0
+
+czy_zliczac = False
+
+for znak in napis:
+    if znak == '<':
+        czy_zliczac = True
+    elif znak == '>':
+        czy_zliczac = False
+    elif czy_zliczac == True:
+        liczna_znakow += 1
+
+print(f"W napisie '{napis}' znaleziono '{liczna_znakow}' znakow miedzy nawiasami <>")
