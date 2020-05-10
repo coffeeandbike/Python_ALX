e15cf90d0f1e5316787ce8ea50d00e76f2deb26c
coffeeandbike <coffeeandbike@users.noreply.github.com>
2020-05-10 19:38:17 +0200

    making class Announcements: adjustments and zad.txt file

--- Zadania_domowe/zad_4_1.py
+++ Zadania_domowe/zad_4_1.py
@@ -11,11 +11,21 @@ m.in. tytuł, opis, cenę, dane kontaktowe sprzedawcy.
 
 
 class Announcements():
-    def __init__(self, title: str, description: str, price: float, seller: dict):
+    def __init__(self, title: str, description: str, price: float, seller_name: str, seller_phone):
         """
         making class which will store announcement
         """
         self.title = title
         self.description = description
         self.price = price
-        self.seller = seller
+        self.seller_name = seller_name
+        self.seller_phone = seller_phone
+
+    def __str__(self) -> str:
+        return f' Tytul ogloszenia: {self.title}\n Opis: {self.description}\n Sprzedajacy: {self.seller_name}\n' \
+               f' Kontakt do Sprzedajacego: {self.seller_phone}\n Cena: {self.price}'
+
+ogloszenie = Announcements("Rower", 'Bardzo dobry rower', 2000.0, 'Jan Nowak', '111 222 333')
+
+
+print(ogloszenie)
