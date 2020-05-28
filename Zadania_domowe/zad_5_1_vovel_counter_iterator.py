ac0797131f64bf87f5651a8ae11c5be9dea49212
coffeeandbike <coffeeandbike@users.noreply.github.com>
2020-05-28 12:29:29 +0200

    zad_5_1 done

---
+++ Zadania_domowe/zad_5_1_vovel_counter_iterator.py
@@ -0,0 +1,54 @@
+'''
+Zaimplementuj iterator zwracający jedynie samogłoski z zadanego ciągu znaków:
+​
+Przykładowe użycie:
+for char in Vowels('ala ma kota a kot ma ale'):
+'''
+
+
+class VolelCouner:
+    VOVELS = ['e', 'y', 'u', 'i', 'o', 'a']
+
+    def __init__(self, text_to_count: str):
+        """
+        class for counting vovels in the string using iterator
+        :type text_to_count: object
+        """
+        self.text_to_count = text_to_count
+
+    def __iter__(self):
+
+        print("Iteration started")
+        self.char_counter = 0
+        return self
+
+    def __next__(self):
+        print("Next")
+
+        self.string_lenght = len(self.text_to_count)
+        self.how_much_vovels = 0
+
+        if self.char_counter > self.string_lenght:
+            raise StopIteration
+
+
+        for char in self.text_to_count:
+            self.char_counter += 1
+            if char in self.__class__.VOVELS:
+                self.how_much_vovels += 1
+            else:
+                self.char_counter += 1
+
+        return f'Amount of vovels: {self.how_much_vovels}'
+
+
+zlicz = VolelCouner("co to ma byc do cholery")
+
+for char in zlicz:
+    print(char)
+
+print('=' * 60)
+
+zlicz2 = VolelCouner("lorem impum lorem ipsum lorem impus")
+for char in zlicz2:
+    print(char)
