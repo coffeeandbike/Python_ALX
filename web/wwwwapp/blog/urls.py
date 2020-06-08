de3fc3fd130908e83d0b4869c7c1fed889dc2361
coffeeandbike <coffeeandbike@users.noreply.github.com>
2020-06-08 08:06:22 +0200

    webapp intro

---
+++ web/wwwwapp/blog/urls.py
@@ -0,0 +1,12 @@
+# wwwwapp/blog/urls.py
+
+from django.urls import path
+
+from blog.views import test, homepage, post_show
+
+urlpatterns = [
+    path('', homepage),
+    path('test/<imie>/<int:wiek>', test),
+    path('post/<slug>', post_show, name='post_show'),
+]
+
